68eda1fd0ca5aa65abc3fa3b9f598cce3858d4f1
Luke Chen <luke.chen.2010@gmail.com>
2020-05-07 19:05:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,18 +4,17 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  @@parse = []
   
   def initialize(emails_csv)
     emails_array = emails_csv.gsub(",", " ").split(" ")
     
     emails_array.each do |email|
-      @@parse << email if !(@@parse.include?(email))
+    @parse << email if !(@parse.include?(email))
     end
   end
    
   def parse
-    return @@parse
+    return @parse
   end
   
 end
